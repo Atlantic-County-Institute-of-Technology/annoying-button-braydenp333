18fa7ea07ef313eff7e924d28b40b584a37fb689
braydenp333 <papa0313@student.acitech.org>
2025-03-07 09:02:47 -0500

    complete

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 let text = document.getElementById("text")
 let button1 = document.getElementById("button1")
 let container = document.getElementById("container")
-
-
-/* let yay= new Audio('yay.mp3'); */
+let clicky = new Audio('assets/click.mp3');
 
 let clicks= 0;
+/*let clicksbutton2= 0;*/
 
-button1.addEventListener("clicks", annoy);
+button1.addEventListener("click", annoy); 
 
 function annoy () {
-    clicks++;
-    alert(clicks)
+    clicks ++;
+    clicky.play()
     console.log(clicks)
     if( clicks == 1 ) {
         text.innerHTML = "Hey you";
     }
-    else if( clicks == 2 ) {
+    else if( clicks == 3 ) {
         button1.classList.add("angry");
-        text.innerHTML = "Stop touching me";
+        text.innerHTML = "Stop touching me please";
     }
-    else if ( clicks == 5 ) {
-    button1.classList.remove("angry");
-    text.innerHTML = "I take a chill pill"
+    else if ( clicks == 7 ) {
+        button1.classList.remove("angry");
+        text.innerHTML = "Ok fine"
     }
 
-    else if( clicks == 20 ) {
-        text.innerHTML = "Breh";
+    else if( clicks == 15 ) {
+        text.innerHTML = "Come click my boy over here then";
         const button2 = document.createElement("button");
-        button2.innerHTML = "Hi My name is Button";
+        button2.innerHTML = "Hi My name also Button";
         button2.classList.add("button1");
         button2.classList.add("button2");
-        container.appendChild("button2");
-        /* yay.play() */
+        container.appendChild(button2);
     }
 }
+
+
